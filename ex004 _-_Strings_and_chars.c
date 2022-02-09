0e338f4984d336344c51299f5892b537da5a0d62
Antonio Fernando <96031071+antoniofernandodj@users.noreply.github.com>
2022-02-09 16:43:14 -0500

    include puts()

--- ex004 _-_Strings_and_chars.c
+++ ex004 _-_Strings_and_chars.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     char str1[] = "Phrase";
 
-    printf("%s\n", str1);
+    printf("%s\n\n", str1);
+    
+    puts(str1);
     printf("%c\n", str1[1]);
     printf("%c\n", str1[2]);
     printf("%c\n", str1[3]);
     printf("%c\n", str1[4]);
     printf("%c\n", str1[5]);
+
     return 0;
 }
