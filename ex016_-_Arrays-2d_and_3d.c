c2a5e67419c1dd935a05f628e006a0e93656f51f
Antonio Fernando <96031071+antoniofernandodj@users.noreply.github.com>
2022-02-09 16:38:36 -0500

    make it better

--- ex016_-_Arrays-2d_and_3d.c
+++ ex016_-_Arrays-2d_and_3d.c
@@ -19,21 +19,35 @@ int main() {
     };
 
     int i, j, k;
+    printf("{\n");
     for (i = 0; i < 2; i++) {
+        printf("\t{\n");
         for (j = 0; j < 3; j++) {
+            printf("\t\t{ ");
             for (k = 0; k < 4; k++) {
-                printf("%d\t", nums3d[i][j][k]);
-            } printf("\n"); k = 0;
-        } printf("\n"); j = 0;
-    } printf("\n"); i = 0;
+                printf("\t %d\t", nums3d[i][j][k]);
+            }
+            printf("\t}\n"); k = 0;
+        }
+        printf("\t}\n"); j = 0;
+    }
+    printf("{\n"); i = 0;
 
+    printf("\n");
+
+    printf("{\n");
     for (i = 0; i < 2; i++) {
+        printf("\t{\n");
         for (j = 0; j < 3; j++) {
+            printf("\t\t{ ");
             for (k = 0; k < 4; k++) {
-                printf("%p\t", &nums3d[i][j][k]);
-            } printf("\n"); k = 0;
-        } printf("\n"); j = 0;
-    } printf("\n"); i = 0;
+                printf("\t [%p]\t", &nums3d[i][j][k]);
+            }
+            printf("\t}\n"); k = 0;
+        }
+        printf("\t}\n"); j = 0;
+    }
+    printf("{\n"); i = 0;
 
     return 0;
 }
