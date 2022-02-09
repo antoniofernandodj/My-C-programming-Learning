90f3c05773bdb9799dc7cc89db27069e9d9a229f
Antonio Fernando <96031071+antoniofernandodj@users.noreply.github.com>
2022-02-09 11:32:17 -0500

    include format code for scanf function

--- ex001_-_Hello-world_printf.c
+++ ex001_-_Hello-world_printf.c
@@ -5,5 +5,25 @@ int main () {
     printf("Hello World!\n");
     printf("My first program!\n");
     printf("Testing printf command\n");
+
+    /*
+    %c	Read a character
+    %d	Read a decimal integer
+    %ld	Read a long integer
+    %hd	Read a short integer
+    %e	Read a floating point value
+    %f	Read a floating point value
+    %lf	Read a double number
+    %Lf	Read a long double number
+    %g	Read a floating point value
+    %h	Read a short integer
+    %i	Read a decimal, hexadecimal or octal integer
+    %o	Read an octal integer
+    %s	Read a string
+    %u	Read an unsigned decimal integer
+    %x	Read a hexadecimal integer
+    %[..]	Read a string of word(s)
+    */
+    
     return 0;
 }
