4d5f07aaf30d92a88ccc03e49b71c98b7365cb35
Antonio Fernando <96031071+antoniofernandodj@users.noreply.github.com>
2022-02-09 11:35:01 -0500

    Format code for printf function

--- ex001_-_Hello-world_printf.c
+++ ex001_-_Hello-world_printf.c
@@ -7,6 +7,7 @@ int main () {
     printf("Testing printf command\n");
 
     /*
+    Format code for scanf function
     %c	Read a character
     %d	Read a decimal integer
     %ld	Read a long integer
@@ -23,6 +24,21 @@ int main () {
     %u	Read an unsigned decimal integer
     %x	Read a hexadecimal integer
     %[..]	Read a string of word(s)
+    
+    Format code for printf function
+    %c	Print a single character
+    %d	Print a decimal integer
+    %ld	Print a long integer
+    %hd	Print a short integer
+    %e	Print a floating point value in exponent form
+    %f	Print a floating point value without exponent
+    %lf	Print a double number
+    %Lf	Print a long double number
+    %g	Print a floating point value either e-type or f-type depending on condition.
+    %i	Print a signed decimal integer
+    %o	Print an octal integer, without leading zero
+    %s	Print a string
+    %x	Print a hexadecimal integer, without leading 0x
     */
     
     return 0;
