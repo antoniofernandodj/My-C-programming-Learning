a13b18926bba8d5a09ae850c5998ae878b76b907
Antonio Fernando <96031071+antoniofernandodj@users.noreply.github.com>
2022-02-09 19:10:06 -0500

    include strlen()

--- ex004_-_Strings_and_chars.c
+++ ex004_-_Strings_and_chars.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     char str1[] = "Phrase";
 
     printf("%s\n\n", str1);
-    
+
     puts(str1);
     printf("%c\n", str1[1]);
     printf("%c\n", str1[2]);
     printf("%c\n", str1[3]);
     printf("%c\n", str1[4]);
     printf("%c\n", str1[5]);
+
+    printf("The length of str1[] = %ld\n\n", strlen(str1));
+
+    char string[] = "Hello";
+    int i;
+    i = 0;
+    while (string[i] != '\0') {
+        printf("%c\n", string[i]);
+        i++;
+    }
 
     return 0;
 }
