d079596cff1fba7fd0b0302c3846328aadde70b3
Antonio Fernando <96031071+antoniofernandodj@users.noreply.github.com>
2022-02-09 19:19:38 -0500

    include concat and compare

--- ex004_-_Strings_and_chars.c
+++ ex004_-_Strings_and_chars.c
@@ -3,19 +3,25 @@
 #include <string.h>
 
 int main() {
+
+    // Declare strings
     char str1[] = "Phrase";
 
+    // Print String
     printf("%s\n\n", str1);
-
     puts(str1);
+
+    // Accessing characters of string
     printf("%c\n", str1[1]);
     printf("%c\n", str1[2]);
     printf("%c\n", str1[3]);
     printf("%c\n", str1[4]);
     printf("%c\n", str1[5]);
 
+    // Length of strings
     printf("The length of str1[] = %ld\n\n", strlen(str1));
 
+    // Iterate strings
     char string[] = "Hello";
     int i;
     i = 0;
@@ -24,5 +30,35 @@ int main() {
         i++;
     }
 
+    // Compare strings
+    char var1[100], var2[100];
+  
+    //input
+    printf("Enter string 1: ");
+    scanf("%s", var1);
+
+    printf("Enter string 2: ");
+    scanf("%s", var2);
+
+    if (strcmp(var1, var2) == 0) {
+        printf("Both are same.\n");
+    } else {
+        printf("Both are different.\n");
+    }
+
+    // Concatenate strings
+    //variable
+    char
+        str1[] = "Hello",
+        str2[] = "World",
+        str3[100] = "";
+    
+    //concat
+    strcat(str3, str1);  //concat "Hello" to str3 so, str3 = "Hello"
+    strcat(str3, " ");   //concat " " to str3     so, str3 = "Hello "
+    strcat(str3, str2);  //concat "World" to str3 so, str3 = "Hello World"
+  
+    printf("Concatenated string: %s\n", str3);
+    
     return 0;
 }
